38da84f520bb97fab0310e70f81e0fee3addf151
Peter Tatkowski <tatp22@gmail.com>
2020-10-31 19:44:37 +0100

    So we don't get scammed

--- src/main/kotlin/ch/homegate/crawler/listingsrequest.kt
+++ src/main/kotlin/ch/homegate/crawler/listingsrequest.kt
@@ -3,7 +3,7 @@ package ch.homegate.crawler
 val listingsRequest = ListingsRequest(
         query = ListingsQuery(
                 location = Location(listOf("geo-city-zurich")),
-                monthlyRent = IntRange(to = 5000),
+                monthlyRent = IntRange(from = 2500, to = 5000),
                 numberOfRooms = IntRange(from = 4),
                 offerType = "RENT",
         )
